29e23e8fed49f2441c04ba8cac6ad33e05e3629c
bovojon <bovojon@gmail.com>
2016-06-15 15:57:27 -0400

    Add NotesController

---
+++ app/app.js
@@ -0,0 +1,15 @@
+(function(){
+	var app = angular.module('meganote', [
+		'ui.router',
+		'meganote.notes'
+	]);
+
+	function config($urlRouterProvider){
+		$urlRouterProvider.otherwise('/notes');
+	}
+
+	config.$inject = ['$urlRouterProvider'];
+	app.config(config);
+
+
+})();
