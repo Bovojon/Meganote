d6fd1257d3057bd6feb6de41083054584ca7d3bf
bovojon <bovojon@gmail.com>
2016-06-15 16:07:12 -0400

    Add note form

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -6,10 +6,16 @@ angular
 
 .config(function($stateProvider){
   $stateProvider
+  //State if url entered is /notes
   .state('notes', {
     url: '/notes',
     templateUrl: 'notes/notes.html',
     controller: 'NotesController'
+  })
+  //State if url entered is /idOfNote
+  .state('notes.form', {
+    url: '/:noteId',
+    templateUrl: 'notes/notes-form.html'
   });
 })
 
