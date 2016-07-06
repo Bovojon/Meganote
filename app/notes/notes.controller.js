233de65b3adeacd32878c8ef9de260ce41272197
bovojon <bovojon@gmail.com>
2016-07-06 10:05:40 -0400

    Rename notes controller file.

---
+++ app/notes/notes.controller.js
@@ -0,0 +1,9 @@
+(function() {
+  angular.module('meganote.notes')
+    .controller('NotesController', NotesController);
+
+  NotesController.$inject = ['$scope', 'NotesService'];
+  function NotesController($scope, NotesService) {
+    $scope.notes = NotesService.notes;
+  }
+}());
