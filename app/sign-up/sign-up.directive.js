3f2cc677d51aefcff45b7ffdd0e1ed625b5c85ff
bovojon <bovojon@gmail.com>
2016-07-14 12:39:06 -0400

    Show flash messages on Sign Up page

--- app/sign-up/sign-up.directive.js
+++ app/sign-up/sign-up.directive.js
@@ -3,17 +3,34 @@
     .directive('signUp', [
 
       '$state',
+      'Flash'
       'UsersService',
-      ($state, UsersService) => {
+      ($state, Flash, UsersService) => {
 
+        let flash = false;
         class SignUpController {
           constructor() {
             this.user = {};
           }
           submit() {
+            if (Number.isInteger(flash)) {
+              FLash.dismiss(flash);
+              flash = false;
+            }
             UsersService.create(this.user)
               .then(
-                () => $state.go('notes.form', { noteId: undefined })
+                () => $state.go('notes.form', { noteId: undefined }),
+
+                (res) => {
+                  ler errors = '',
+                  for (let errors of res.data.errors) {
+                    errors += `<li>${error}</li>`;
+                  }
+                  flash = Flash.create('danger', `
+                  <p>Oops! Something went wrong.</p>
+ +                <ul>${errors}</ul>
+                  `);
+                }
               );
           }
         }
